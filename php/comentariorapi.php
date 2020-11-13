0dc26bcea417b2226dfccbbafc4a1c2609af95a8
ESNJ14 <siegfried.nah@gmail.com>
2020-11-12 22:11:07 -0700

    first commit

---
+++ php/comentariorapi.php
@@ -0,0 +1,21 @@
+<?php 
+	$name = "ESAUL FIGUEROA MORGAN";
+?>
+
+
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Sitio Web</title> 
+</head>
+
+<body>
+
+	<h1>
+		hola mi nombre es <?= $name;?>
+	</h1>
+
+
+</body>
+</html>
---
+++ php/ifelse.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Sitio Web</title> 
+</head>
+
+<body>
+
+	<?php if (true): ?>
+		<p>
+			aldasdijs
+		</p>
+	<?php else: ?>	
+	<p>
+		ssssssssssssssssss
+	</p>
+	<?php endif ?>
+
+
+</body>
+</html>
